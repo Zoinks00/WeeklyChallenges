2cc87bc58821c575f6b82e629937410ae8e5a397
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-04-06 02:09:14 -0700

    Weekly challange3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +8,107 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach( var item in vals)
+            {
+                if(item == false)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+            var oddSum = numbers.Sum();
+
+            return (oddSum % 2 != 0);
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool isLower = false;
+            bool isUpper = false;
+            bool isNumber = false;
+
+            for (int x = 0; x < password.Length; x++)
+            {
+                if (char.IsUpper(password[x])) //check single element of current passwoard index for UpperCase letter
+                {                             // set isUpper value to true if current element has UpperCase  
+                    isUpper = true;
+                }
+                if (char.IsLower(password[x]))
+                {
+                    isLower = true;
+                }
+                if (char.IsNumber(password[x]))
+                {
+                    isNumber = true;
+                }
+            }
+                //check all variables at onces to see if any have false value
+                // return false if any of the above check fails
+                if( isNumber == true && isLower == true && isUpper == true )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val.First();
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            return val[val.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if(divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
+
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+
+            for(int i = 0;i < 100; i++)
+            {
+                if(i % 2 !=0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+           for(int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+
         }
     }
 }
